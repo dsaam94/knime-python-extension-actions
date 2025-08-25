125e0a636bb18e562a04556548edb14c688a6449
Ali Marvi <ali.marvi@knime.com>
2025-08-25 18:12:54 +0200

    added check for secret leaks

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -6,7 +6,7 @@ from pandas.testing import assert_frame_equal
 
 from src.extension import TemplateNode
 
-API_KEY = "gdgfdgfd24345)(&)KJFFFGKJ"
+password = "1233374628742"
 
 
 class TestTemplateNode(unittest.TestCase):
